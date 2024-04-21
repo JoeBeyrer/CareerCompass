86a3733cf1d0a1ceb8649d84a82145079390838f
Anxhelo Kambo <88976322+ChimerG@users.noreply.github.com>
2024-04-21 04:23:35 -0400

    Update Posthomepage.py

--- careercompass/careercompassapp/Posthomepage.py
+++ careercompass/careercompassapp/Posthomepage.py
@@ -1,18 +1,37 @@
+
 from django.shortcuts import render
-from .models import Post  # Import your Post model
-from .database_utils import get_post_likes, get_user_type
+from .models import Posts, Users  # Import relevant models
+from .database_utils import get_user_type, get_following_posts, get_post_likes
 
 def index(request):
+    # Get the current user's username
     current_user = request.user.username
+    
+    # Determine the type of user (e.g., student or recruiter)
     user_type = get_user_type(current_user)
     
-    # Retrieve posts from the database
-    posts = Post.objects.all()  # Adjust this query based on your Post model structure and any filtering you need
+    # Retrieve posts from the database for the current user's following list
+    following_posts = get_following_posts(current_user)
     
+    # Prepare the post data to be passed to the template
     post_data = []
-    for post in posts:
-        likes_count = get_post_likes(post.posted_by, post.date_posted)  # Adjust parameters accordingly
-        poster_type = get_user_type(post.posted_by)
-        post_data.append({'post': post, 'likes_count': likes_count, 'user_type': poster_type})
+    for post in following_posts:
+        # Get the number of likes for each post
+        likes_count = get_post_likes(post.PostedBy, post.DatePosted)
+        
+        # Get the user type who posted the current post
+        posted_by_user = Users.objects.get(UserID=post.PostedBy)
+        poster_type = get_user_type(posted_by_user.UserID)
+        
+        # Construct a dictionary containing post details and related data
+        post_info = {
+            'post': post,               # The Post object itself
+            'likes_count': likes_count, # Number of likes for the post
+            'user_type': poster_type    # Type of user who posted the post
+        }
+        
+        # Append the post info to the post_data list
+        post_data.append(post_info)
     
+    # Render the index.html template with the user type and post data
     return render(request, 'index.html', {'type': user_type, 'posts': post_data})
