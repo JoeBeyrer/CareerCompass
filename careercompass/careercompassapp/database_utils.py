3685622ed248bca32af3643b02b5fd6ccdfa8acd
Joe Beyrer <joebeyrer@gmail.com>
2024-04-26 22:17:25 -0400

    Added comments to queries

--- careercompass/careercompassapp/database_utils.py
+++ careercompass/careercompassapp/database_utils.py
@@ -14,9 +14,9 @@ def add_user(UserID, FirstName, LastName, PasswordHash, Email, AboutMe=None):
             [UserID, FirstName, LastName, PasswordHash, AboutMe, Email]
         )
 
+# Add the student to the students table
 def add_student(UserID, University, Degree, CurrentYear, ExpectedGraduation, GPA, OpenToWork):
      with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
         cursor.execute(
             """
             INSERT INTO students VALUES (%s, %s, %s, %s, %s, %s, %s);
@@ -24,9 +24,9 @@ def add_student(UserID, University, Degree, CurrentYear, ExpectedGraduation, GPA
             [UserID, University, Degree, CurrentYear, ExpectedGraduation, GPA, OpenToWork]
         )
 
+# Add the recruiter to the recruiter table
 def add_recruiter(UserID, CompanyName, AboutCompany, Position):
-     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+     with connection.cursor() as cursor:       
         cursor.execute(
             """
             INSERT INTO recruiters VALUES (%s, %s, %s, %s);
@@ -37,23 +37,22 @@ def add_recruiter(UserID, CompanyName, AboutCompany, Position):
 # Get the user information from the database
 def get_user(UserID):
      with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
         cursor.execute(
             """
             SELECT * FROM users WHERE users.UserID = %s;
             """,
             [UserID]
         )
-        # Fetch the row with the correct UserID
+        
         user_data = cursor.fetchone()
         if user_data:
             return user_data
         else:
             return None
 
+# Get the students data from both the students and users tables
 def get_student_data(UserID):
-    with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+    with connection.cursor() as cursor: 
         cursor.execute(
             """
             SELECT * FROM students JOIN users 
@@ -62,16 +61,16 @@ def get_student_data(UserID):
             """,
             [UserID]
         )
-        # Fetch the row with the correct UserID
+        
         student_data = cursor.fetchone()
         if student_data:
             return student_data
         else:
             return None
 
+# Get the recruiters data from both the recruiters and users tables
 def get_recruiter_data(UserID):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
         cursor.execute(
             """
             SELECT * FROM recruiters JOIN users 
@@ -80,49 +79,50 @@ def get_recruiter_data(UserID):
             """,
             [UserID]
         )
-        # Fetch the row with the correct UserID
+        
         recruiter_data = cursor.fetchone()
         if recruiter_data:
             return recruiter_data
         else:
             return None
 
+# Counts how many times the users ID shows as being followed by another user in the followers database
 def get_follower_count(UserID):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL 
-        # injections
         cursor.execute(
             """
             SELECT COUNT(*) FROM followers WHERE followers.UserID = %s;
             """,
             [UserID]
         )
-        # Fetch the row with the correct UserID
+        
         followers = cursor.fetchone()
         if followers:
             return followers
         else:
             return None
 
+# Counts how many times the users ID shows as following another user in the followers database
 def get_following_count(UserID):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             SELECT COUNT(*) FROM followers WHERE followers.FollowerID = %s;
             """,
             [UserID]
         )
-        # Fetch the row with the correct UserID
+        
         following = cursor.fetchone()
         if following:
             return following
         else:
             return None
-        
+
+# Adds the post to the database with the date posted set to the current timestamp of NOW()        
 def create_post(PostedBy, Title, BodyText, Field):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             INSERT INTO posts VALUES (%s, NOW(), %s, %s, %s);
@@ -130,6 +130,7 @@ def create_post(PostedBy, Title, BodyText, Field):
             [PostedBy, Title, BodyText, Field]
         )
 
+# Removes the post from the database using the userID and the timestamp it was posted
 def delete_post(PostedBy, DatePosted):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -139,9 +140,10 @@ def delete_post(PostedBy, DatePosted):
             [PostedBy, DatePosted]
         )
 
+# Adds the userID and ID of the follower to the database when one user follows another
 def follow(UserID, FollowerID):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             INSERT INTO followers VALUES (%s, %s);
@@ -149,9 +151,10 @@ def follow(UserID, FollowerID):
             [UserID, FollowerID]
         )
 
+# Removes the follower from the follows database when a user unfollows another
 def unfollow(UserID, FollowerID):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             DELETE FROM followers WHERE UserID=%s AND FollowerID=%s;
@@ -159,6 +162,7 @@ def unfollow(UserID, FollowerID):
             [UserID, FollowerID]
         )
 
+# Adds like to likes table using current users ID, the posters ID, and the date the post was made
 def liked(UserID, PostedBy, DatePosted):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -168,9 +172,10 @@ def liked(UserID, PostedBy, DatePosted):
             [UserID, PostedBy, DatePosted]
         )
 
+# Removes the like from likes in the event of a user unliking a post
 def unlike(UserID, PostedBy, DatePosted):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             DELETE FROM likes WHERE UserID=%s AND PostedBy=%s AND DatePosted=%s;
@@ -178,6 +183,7 @@ def unlike(UserID, PostedBy, DatePosted):
             [UserID, PostedBy, DatePosted]
         )
 
+# Returns a list of all the userIDs from followers where the user being followed is the input provided
 def following_list(UserID):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -192,6 +198,7 @@ def following_list(UserID):
         else:
             return None
 
+# Returns a list of all the userIDs from followers where the user following another is the input provided
 def get_user_type(UserID):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -211,9 +218,10 @@ def get_user_type(UserID):
         else:
             return None
 
+# Selects all posts made by a specific user
 def get_user_posts(UserID):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             SELECT * FROM posts WHERE PostedBy=%s ORDER BY DatePosted DESC;
@@ -226,6 +234,8 @@ def get_user_posts(UserID):
         else:
             return None
 
+# Selects all posts made by users the current user is following. This is done by joining posts and followers on the followerID
+# Only posts within the past year will show.
 def get_following_posts(UserID):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -242,10 +252,11 @@ def get_following_posts(UserID):
             return posts
         else:
             return None
-    
+
+# Returns the number of likes for a specific post 
 def get_like_count(PostedBy, DatePosted):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             SELECT COUNT(*) FROM likes WHERE PostedBy=%s AND DatePosted=%s;
@@ -258,9 +269,10 @@ def get_like_count(PostedBy, DatePosted):
         else:
             return None
 
+# Checks if the current user has already liked a post
 def check_liked_post(UserID, PostedBy, DatePosted):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             SELECT * FROM likes WHERE UserID=%s AND PostedBy=%s AND DatePosted=%s;
@@ -272,10 +284,11 @@ def check_liked_post(UserID, PostedBy, DatePosted):
             return posts
         else:
             return None
-        
+
+# Gets a post using the id of the user who posted it and the timestamp it was posted        
 def get_post(PostedBy, DatePosted):
     with connection.cursor() as cursor:
-        # Using cursor.execute with %s fields keeps the query safe from SQL injections
+        
         cursor.execute(
             """
             SELECT * FROM posts WHERE PostedBy=%s and DatePosted=%s;
@@ -288,6 +301,8 @@ def get_post(PostedBy, DatePosted):
         else:
             return None
 
+# Search for a user in users using an input string compared to their userID, first name, and last name.
+# Only the 30 results with the shortest UserIDs are returned to ensure the closest match by username
 def search_user(input):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -305,6 +320,7 @@ def search_user(input):
         else:
             return None
 
+# Retrieves the user data for the followers of a provided user by using a JOIN of followers and users
 def get_followers(UserID):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -321,6 +337,7 @@ def get_followers(UserID):
         else:
             return None
 
+# Retrieves the user data for the users followed by the provided user using a JOIN of followers and users
 def get_following(UserID):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -337,6 +354,7 @@ def get_following(UserID):
         else:
             return None
 
+# Gets the total likes across all posts made by a user
 def get_user_likes_count(UserID):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -351,6 +369,8 @@ def get_user_likes_count(UserID):
         else:
             return 0  # Return 0 if no likes found for the user
 
+# Updates user data using casestatements so that not all inputs are required.
+# If the input is NULL, we do not change the data, otherwise it is updated to the new value
 def update_user(UserID, FirstName, LastName, PasswordHash, AboutMe, Email, current_user):
     with connection.cursor() as cursor:
         UserID = None if UserID == "" else UserID
@@ -377,6 +397,7 @@ def update_user(UserID, FirstName, LastName, PasswordHash, AboutMe, Email, curre
             
         )
 
+# Updates all student specific data in the students table
 def update_student(University, Degree, CurrentYear, ExpectedGraduation, GPA, OpenToWork, current_user):
     with connection.cursor() as cursor:
         University = None if University == "" else University
@@ -402,6 +423,7 @@ def update_student(University, Degree, CurrentYear, ExpectedGraduation, GPA, Ope
 
         )
 
+# Updates all recruiter specific data in the recruiters table
 def update_recruiter(CompanyName, AboutCompany, Position, current_user):
     with connection.cursor() as cursor:
         CompanyName = None if CompanyName == "" else CompanyName
@@ -420,6 +442,7 @@ def update_recruiter(CompanyName, AboutCompany, Position, current_user):
              Position, Position, current_user]
         )
 
+# Checks if the email has already been used by another user for input validation  
 def check_email(email):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -434,7 +457,8 @@ def check_email(email):
             return True
         else:
             return False  
-        
+
+# Checks if the username already exists in the table for input validation        
 def check_username(UserID):
     with connection.cursor() as cursor:
         cursor.execute(
@@ -449,6 +473,7 @@ def check_username(UserID):
         else:
             return False  
 
+# Deletes the users data (cascades) using the userID
 def delete_user(UserID):
      with connection.cursor() as cursor:
         cursor.execute(
